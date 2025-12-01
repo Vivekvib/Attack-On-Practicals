49ab9a074b16a1f9847e4e5bff66576241045e39
Vivek Nishad <98114443+Vivekvib@users.noreply.github.com>
2025-12-01 22:23:41 +0530

    Add files via upload

---
+++ Theory_Of_Computation/Write a program to simulate an FA that accepts: a. Union of the languages L1 and L2 b. Intersection of the languages L1 and L2 c. Language L1 L2 (concatenation)/FA_6-2.cpp
@@ -0,0 +1,68 @@
+//FA that is a concatenation of two languages:- L1: words starting with a & L2: words ending with a
+
+#include <iostream>
+using namespace std;
+
+class FA{
+    public:
+        void state1(string str, int index){
+            //initial state
+            if(str.length()==index){
+                cout<<"String Rejected!"<<endl;
+                return;
+            }
+            else{
+                if(str[index] == 'a'){
+                    state2(str, index+1);
+                }
+                else{
+                    state4(str, index+1);
+                }
+            }
+            
+        }
+        void state2(string str, int index){
+            //transition state
+            if(str.length()==index){
+                cout<<"String Rejected!"<<endl;
+                return;
+            }
+            else{
+                if(str[index] == 'a'){
+                    state3(str, index+1);
+                }
+                else{
+                    state2(str, index+1);
+                }
+            }
+        }
+        void state3(string str, int index){
+            //final state
+            if(str.length() == index){
+                cout<<"String Accepted"<<endl;
+                return;
+            }
+            else{
+                if(str[index] == 'a'){
+                    state3(str, index+1);
+                }
+                else{
+                    state2(str, index+1);
+                }
+            }
+        }
+        void state4(string str, int index){
+            //dead state
+            cout<<"String Rejected"<<endl;
+        }
+};
+
+int main(){
+    string str;
+    cout<<"Enter your string: "<<endl;
+    getline(cin,str);
+
+    FA f;
+    f.state1(str,0);
+    return 0;
+}
