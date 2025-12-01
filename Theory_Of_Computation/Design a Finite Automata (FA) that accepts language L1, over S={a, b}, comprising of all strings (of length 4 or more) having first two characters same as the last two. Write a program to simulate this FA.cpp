2d7286ae1cc1afeb2ef85658fd61fd9205c62384
Vivek Nishad <98114443+Vivekvib@users.noreply.github.com>
2025-12-01 22:53:54 +0530

    Update Design a Finite Automata (FA) that accepts language L1, over S={a, b}, comprising of all strings (of length 4 or more) having first two characters same as the last two. Write a program to simulate this FA.cpp

--- Theory_Of_Computation/Design a Finite Automata (FA) that accepts language L1, over S={a, b}, comprising of all strings (of length 4 or more) having first two characters same as the last two. Write a program to simulate this FA.cpp
+++ Theory_Of_Computation/Design a Finite Automata (FA) that accepts language L1, over S={a, b}, comprising of all strings (of length 4 or more) having first two characters same as the last two. Write a program to simulate this FA.cpp
@@ -1,115 +1,147 @@
-#include <iostream> 
-#include <string>   
-#include <map>     
-#include <set>     
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
 using namespace std;
-teDFA(const string& inputString) {
-    set<char> alphabet = {'a', 'b'};
-    map<string, map<char, string>> transitions;
-    transitions["q0"] = {{'a', "S_a"}, {'b', "S_b"}};
-    transitions["S_a"] = {{'a', "S_aa"}, {'b', "S_ab"}};
-    transitions["S_b"] = {{'a', "S_ba"}, {'b', "S_bb"}};
-    transitions["S_aa"] = {{'a', "S_aa_L3a"}, {'b', "S_aa_L3b"}};
-    transitions["S_ab"] = {{'a', "S_ab_L3a"}, {'b', "S_ab_L3b"}};
-    transitions["S_ba"] = {{'a', "S_ba_L3a"}, {'b', "S_ba_L3b"}};
-    transitions["S_bb"] = {{'a', "S_bb_L3a"}, {'b', "S_bb_L3b"}};
-    transitions["S_aa_L3a"] = {{'a', "S_aa_AA"}, {'b', "S_aa_AB"}};
-    transitions["S_aa_L3b"] = {{'a', "S_aa_BA"}, {'b', "S_aa_BB"}};
-    transitions["S_ab_L3a"] = {{'a', "S_ab_AA"}, {'b', "S_ab_AB"}}; 
-    transitions["S_ab_L3b"] = {{'a', "S_ab_BA"}, {'b', "S_ab_BB"}};
-    transitions["S_ba_L3a"] = {{'a', "S_ba_AA"}, {'b', "S_ba_AB"}}; 
-    transitions["S_ba_L3b"] = {{'a', "S_ba_BA"}, {'b', "S_ba_BB"}}; 
-    transitions["S_bb_L3a"] = {{'a', "S_bb_AA"}, {'b', "S_bb_AB"}}; 
-    transitions["S_bb_L3b"] = {{'a', "S_bb_BA"}, {'b', "S_bb_BB"}}; 
-    // The logic is: S_xx_YZ on 'c' -> S_xx_Zc
-    transitions["S_aa_AA"] = {{'a', "S_aa_AA"}, {'b', "S_aa_AB"}};
-    transitions["S_aa_AB"] = {{'a', "S_aa_BA"}, {'b', "S_aa_BB"}};
-    transitions["S_aa_BA"] = {{'a', "S_aa_AA"}, {'b', "S_aa_AB"}}; 
-    transitions["S_aa_BB"] = {{'a', "S_aa_BA"}, {'b', "S_aa_BB"}}; 
-    transitions["S_ab_AA"] = {{'a', "S_ab_AA"}, {'b', "S_ab_AB"}};
-    transitions["S_ab_AB"] = {{'a', "S_ab_BA"}, {'b', "S_ab_BB"}};
-    transitions["S_ab_BA"] = {{'a', "S_ab_AA"}, {'b', "S_ab_AB"}};
-    transitions["S_ab_BB"] = {{'a', "S_ab_BA"}, {'b', "S_ab_BB"}};
-    transitions["S_ba_AA"] = {{'a', "S_ba_AA"}, {'b', "S_ba_AB"}};
-    transitions["S_ba_AB"] = {{'a', "S_ba_BA"}, {'b', "S_ba_BB"}};
-    transitions["S_ba_BA"] = {{'a', "S_ba_AA"}, {'b', "S_ba_AB"}};
-    transitions["S_ba_BB"] = {{'a', "S_ba_BA"}, {'b', "S_ba_BB"}};
-    transitions["S_bb_AA"] = {{'a', "S_bb_AA"}, {'b', "S_bb_AB"}};
-    transitions["S_bb_AB"] = {{'a', "S_bb_BA"}, {'b', "S_bb_BB"}};
-    transitions["S_bb_BA"] = {{'a', "S_bb_AA"}, {'b', "S_bb_AB"}};
-    transitions["S_bb_BB"] = {{'a', "S_bb_BA"}, {'b', "S_bb_BB"}};
-    string startState = "q0";
-    set<string> finalStates = {
-        "S_aa_AA", 
-        "S_ab_AB", 
-        "S_ba_BA", 
-        "S_bb_BB"
-    };
-    string currentState = startState;
-    cout << "Input: '" << inputString << "'" << endl;
-    if (inputString.empty()) {
-        cout << "Finished processing. Final state is: " << currentState << endl;
-        cout << "Result: Rejected\n" << endl;
-        return false;
+class DFA {
+private:
+    static const int S_START = 0;
+    static const int S_A = 1;
+    static const int S_B = 2
+    static const int S_AA = 3;
+    static const int S_AB = 4;
+    static const int S_BA = 5;
+    static const int S_BB = 6;
+    static const int S_L3_AA_A = 7;
+    static const int S_L3_AA_B = 8;
+    static const int S_L3_AB_A = 9;
+    static const int S_L3_AB_B = 10;
+    static const int S_L3_BA_A = 11;
+    static const int S_L3_BA_B = 12;
+    static const int S_L3_BB_A = 13;
+    static const int S_L3_BB_B = 14;
+    static const int LOOP_AA_aa = 15; // Accepting (Matches AA)
+    static const int LOOP_AA_ab = 16;
+    static const int LOOP_AA_ba = 17;
+    static const int LOOP_AA_bb = 18;
+    static const int LOOP_AB_aa = 19;
+    static const int LOOP_AB_ab = 20; // Accepting (Matches AB)
+    static const int LOOP_AB_ba = 21;
+    static const int LOOP_AB_bb = 22;
+    static const int LOOP_BA_aa = 23;
+    static const int LOOP_BA_ab = 24;
+    static const int LOOP_BA_ba = 25; // Accepting (Matches BA)
+    static const int LOOP_BA_bb = 26;
+    static const int LOOP_BB_aa = 27;
+    static const int LOOP_BB_ab = 28;
+    static const int LOOP_BB_ba = 29;
+    static const int LOOP_BB_bb = 30;
+    static const int TOTAL_STATES = 31;
+    vector<vector<int>> transitionTable;
+    vector<bool> finalStates;
+    vector<string> stateDescriptions;
+public:
+    DFA() {
+        transitionTable.resize(TOTAL_STATES, vector<int>(2)); 
+        finalStates.resize(TOTAL_STATES, false);
+        stateDescriptions.resize(TOTAL_STATES);
+        setupDFA();
     }
-    for (char symbol : inputString) {
-        // Check for invalid symbols
-        if (alphabet.find(symbol) == alphabet.end()) {
-            cerr << "Error: Symbol '" << symbol << "' is not in the alphabet {a, b}" << endl;
-            cout << "Result: Rejected\n" << endl;
+    void setupDFA() {
+        stateDescriptions[S_START] = "Start";
+        addTransition(S_START, S_A, S_B);
+        stateDescriptions[S_A] = "Got 'a'";
+        stateDescriptions[S_B] = "Got 'b'";
+        addTransition(S_A, S_AA, S_AB);
+        addTransition(S_B, S_BA, S_BB);
+        stateDescriptions[S_AA] = "Target: AA (Len 2)";
+        stateDescriptions[S_AB] = "Target: AB (Len 2)";
+        stateDescriptions[S_BA] = "Target: BA (Len 2)";
+        stateDescriptions[S_BB] = "Target: BB (Len 2)";
+        addTransition(S_AA, S_L3_AA_A, S_L3_AA_B);
+        addTransition(S_AB, S_L3_AB_A, S_L3_AB_B);
+        addTransition(S_BA, S_L3_BA_A, S_L3_BA_B);
+        addTransition(S_BB, S_L3_BB_A, S_L3_BB_B);
+        // Transitions go to Loop States (Level 4)
+        stateDescriptions[S_L3_AA_A] = "Target AA | Suffix: aa (Len 3)";
+        stateDescriptions[S_L3_AA_B] = "Target AA | Suffix: ab (Len 3)";
+        addTransition(S_L3_AA_A, LOOP_AA_aa, LOOP_AA_ab);
+        addTransition(S_L3_AA_B, LOOP_AA_ba, LOOP_AA_bb);
+        stateDescriptions[S_L3_AB_A] = "Target AB | Suffix: ba (Len 3)"; // Prev was b (from ab), now a -> ba
+        stateDescriptions[S_L3_AB_B] = "Target AB | Suffix: bb (Len 3)";
+        addTransition(S_L3_AB_A, LOOP_AB_aa, LOOP_AB_ab);
+        addTransition(S_L3_AB_B, LOOP_AB_ba, LOOP_AB_bb);
+        stateDescriptions[S_L3_BA_A] = "Target BA | Suffix: aa (Len 3)";
+        stateDescriptions[S_L3_BA_B] = "Target BA | Suffix: ab (Len 3)";
+        addTransition(S_L3_BA_A, LOOP_BA_aa, LOOP_BA_ab);
+        addTransition(S_L3_BA_B, LOOP_BA_ba, LOOP_BA_bb);
+        stateDescriptions[S_L3_BB_A] = "Target BB | Suffix: ba (Len 3)";
+        stateDescriptions[S_L3_BB_B] = "Target BB | Suffix: bb (Len 3)";
+        addTransition(S_L3_BB_A, LOOP_BB_aa, LOOP_BB_ab);
+        addTransition(S_L3_BB_B, LOOP_BB_ba, LOOP_BB_bb);
+        setupLoopBlock(LOOP_AA_aa, "AA", S_AA);
+        setupLoopBlock(LOOP_AB_aa, "AB", S_AB);
+        setupLoopBlock(LOOP_BA_aa, "BA", S_BA);
+        setupLoopBlock(LOOP_BB_aa, "BB", S_BB);
+        finalStates[LOOP_AA_aa] = true;
+        finalStates[LOOP_AB_ab] = true;
+        finalStates[LOOP_BA_ba] = true;
+        finalStates[LOOP_BB_bb] = true;
+    }
+    void addTransition(int from, int toA, int toB) {
+        transitionTable[from][0] = toA;
+        transitionTable[from][1] = toB;
+    }
+    void setupLoopBlock(int startId, string targetName, int targetType) {
+        int id_aa = startId;
+        int id_ab = startId + 1;
+        int id_ba = startId + 2;
+        int id_bb = startId + 3;
+        stateDescriptions[id_aa] = "Target " + targetName + " | Suffix: aa";
+        stateDescriptions[id_ab] = "Target " + targetName + " | Suffix: ab";
+        stateDescriptions[id_ba] = "Target " + targetName + " | Suffix: ba";
+        stateDescriptions[id_bb] = "Target " + targetName + " | Suffix: bb";
+        addTransition(id_aa, id_aa, id_ab);
+        addTransition(id_ab, id_ba, id_bb);
+        addTransition(id_ba, id_aa, id_ab);
+        addTransition(id_bb, id_ba, id_bb);
+    }
+    bool simulate(string input) {
+        int currentState = S_START;
+        cout << "\n--------------------------------------------------" << endl;
+        cout << "Processing: \"" << input << "\"" << endl;
+        cout << "Start: " << stateDescriptions[currentState] << endl;
+        for (char c : input) {
+            if (c != 'a' && c != 'b') {
+                cout << "Error: Invalid char '" << c << "'" << endl;
+                return false;
+            }
+            int inputIdx = (c == 'a') ? 0 : 1;
+            int nextState = transitionTable[currentState][inputIdx];
+            cout << "  Input '" << c << "' -> " << stateDescriptions[nextState] << endl;
+            currentState = nextState;
+        }
+        cout << "End State: " << stateDescriptions[currentState] << endl;
+        if (finalStates[currentState]) {
+            cout << "Result: ACCEPTED" << endl;
+            return true;
+        } else {
+            cout << "Result: REJECTED" << endl;
             return false;
         }
-        if (transitions.find(currentState) == transitions.end()) {
-            cerr << "Error: Reached undefined state: " << currentState << endl;
-            cout << "Result: Rejected\n" << endl;
-            return false;
-        }
-        string nextState = transitions[currentState][symbol];
-        currentState = nextState;
     }
-    bool isAccepted = (finalStates.find(currentState) != finalStates.end());
-    cout << "Finished processing. Final state is: " << currentState << endl;
-    if (isAccepted) {
-        cout << "Result: Accepted\n" << endl;
-    } else {
-        cout << "Result: Rejected\n" << endl;
-    }
-    return isAccepted;
-}
+};
 int main() {
-    string testStrings[] = {
-        "a",
-        "aa",
-        "aba",
-        "aaaa",   
-        "abab",    
-        "baba",   
-        "bbbb",    
-        "aaab",   
-        "bbaa",   
-        "abba",   
-        "aabaa",  
-        "babbb",   
-        "babab",   
-        "bababa", 
-        "bbaba",  
-        "aaaaa"   
-    };
-    int numTests = sizeof(testStrings) / sizeof(testStrings[0]);
-    cout << "--- Running Test Cases ---" << endl;
-    for (int i = 0; i < numTests; ++i) {
-        simulateDFA(testStrings[i]);
-    }
-    cout << "--- Interactive Mode ---" << endl;
-    cout << "Enter 'exit' to quit." << endl;
-    string userInput;
+    DFA dfa;
+    cout << "=== DFA Simulator ===" << endl;
+    cout << "Language: Strings (len >= 4) where first 2 chars == last 2 chars." << endl;
+    cout << "Alphabet: {a, b}" << endl;
+    string input;
     while (true) {
-        cout << "Enter a string of 'a's and 'b's: ";
-        getline(cin, userInput);
-        if (userInput == "exit") {
-            break;
-        }
-        simulateDFA(userInput);
+        cout << "\nEnter string (or 'exit'): ";
+        cin >> input;
+        if (input == "exit") break;
+        dfa.simulate(input);
     }
     return 0;
 }
